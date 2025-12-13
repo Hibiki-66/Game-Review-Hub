dd43e0f2ff6541c23c26f9416ffe743d1648fded
Ubuntu <ubuntu@ip-172-31-9-180.ap-northeast-1.compute.internal>
2025-12-13 07:46:56 +0000

    [Add]投稿ページ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,14 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to posts_path
   end
 
   def edit
@@ -19,4 +24,11 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:review_title, :game_title, :body, :review)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
+
   devise_for :users
   root to: "homes#top"
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
