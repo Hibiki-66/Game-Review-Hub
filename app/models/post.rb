9f96899f8098bd65cd41043443cee7f1cb1c19aa
Ubuntu <ubuntu@ip-172-31-9-180.ap-northeast-1.compute.internal>
2026-01-15 12:34:35 +0000

    [Finish]検索機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,19 @@ class Post < ApplicationRecord
   validates :review_title, presence: true
   validates :game_title, presence: true
   validates :body, presence: true
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @post = Post.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @post = Post.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @post = Post.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @post = Post.where("title LIKE?","%#{word}%")
+    else
+      @post = Post.all
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   resources :posts
   resources :users, only: [:show, :edit, :update, :destroy]
+  get "search" => "searches#search"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
