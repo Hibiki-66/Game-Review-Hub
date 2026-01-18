81c84198de573e1f2aa9be66460c1b49dd819722
Ubuntu <ubuntu@ip-172-31-9-180.ap-northeast-1.compute.internal>
2026-01-18 07:46:39 +0000

    [Finish]コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :posts do
+    resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update, :destroy]
