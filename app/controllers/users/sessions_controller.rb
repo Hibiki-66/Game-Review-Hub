c2ae71d8fac43154ae45f002b97597fb47781e6e
Ubuntu <ubuntu@ip-172-31-9-180.ap-northeast-1.compute.internal>
2026-02-26 10:14:41 +0000

    [Finish]ゲストログイン

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Users::SessionsController < Devise::SessionsController
+  before_action :ensure_guest_user, only: [:edit]
+  
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user), notice: "guestuserでログインしました。"
+  end
+
+  private
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.guest_user?
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,20 @@ class User < ApplicationRecord
     else
       @user = User.all
     end
+
+  end
+
+  GUEST_USER_EMAIL = "guest@example.com"
+
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
+  def guest_user?
+    email == GUEST_USER_EMAIL
   end
   
 end
